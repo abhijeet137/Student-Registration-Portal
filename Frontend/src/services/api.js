2534ba0de1256551132872826ccd1ed5da312ecd
abhijeet137 <wwwabhijeet2003@gmail.com>
2026-08-07 01:16:53 +0530

    Fix API base URL and authentication

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,12 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // Production Backend URL
   baseURL: "https://student-registration-portal-2goy.onrender.com/api",
-
-  // Send HTTP-Only Cookies
   withCredentials: true,
-
   headers: {
     "Content-Type": "application/json",
   },
